1fe06ff864c801c404330670a46f9ec088e18988
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-11-29 16:56:48 -0700

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,15 @@
+// Made by Benjamin Espenschied
+// V1.0
+
+using System;
+
+namespace myProgram;
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
