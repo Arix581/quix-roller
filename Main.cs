b852d680b5c8c95a98a7384257585c8bc83a65e2
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-11-30 20:18:06 -0700

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,15 +1,37 @@
 // Made by Benjamin Espenschied
-// V1.0
+// V1.1
 
 using System;
 
-namespace myProgram;
+namespace myProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Dont close the program");
+        }
+    }
+    
+    class Dice
+    {
+        public int roll;
+        public int sides;
+        public int[] results = new int[2];
+        public string status;
+        public Random numGen = new Random();
+        
+        public Dice(int _sides)
+        {
+            roll = 0;
+            status = "active";
+            sides = _sides;
+        }
+        
+        public int rollDie()
+        {
+            roll = numGen.Next(1, sides + 1);
+            return roll;
         }
     }
 }
