3eeff22e86a87111cced048992d8aaf236854790
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-12-04 10:12:56 -0700

    Update Main.cs
    Added a system for enabling and disabling dice

--- Main.cs
+++ Main.cs
@@ -1,5 +1,7 @@
+
+
 // Made by Benjamin Espenschied
-// V1.1
+// V1.2
 
 using System;
 
@@ -9,7 +11,51 @@ namespace myProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dont close the program");
+            //Console.WriteLine("Dont close the program");
+            string keepGoing = "yes";
+            
+            // Set up dice
+            Dice redDice = new Dice(6);
+            Dice yellowDice = new Dice(6);
+            Dice greenDice = new Dice(6);
+            Dice blueDice = new Dice(6);
+            
+            // Introductions
+            Console.WriteLine("Hello! My name is Josh. I help work the Quixx dice. If \nYou want to roll the dice, just press enter. Typing \nin a dice color or no, then it will disable that \ndice or stop the program. Go ahead! Give it a shot!");
+            
+            // Main loop
+            while (keepGoing != "no") 
+            {
+                keepGoing = Console.ReadLine();
+                switch(keepGoing) 
+                {
+                    case "red":
+                        if (redDice.status == "active"){
+                            redDice.status = "disabled";
+                            Console.WriteLine("Red has been Disabled!");
+                        } else {
+                            redDice.status = "active";
+                            Console.WriteLine("Red has been Activated!");
+                        };
+                        break;
+                    case "yellow":
+                        yellowDice.status = "disabled";
+                        Console.WriteLine("Yellow has been Disabled!");
+                        break;
+                    case "greem":
+                        greenDice.status = "disabled";
+                        Console.WriteLine("Green has been Disabled!");
+                        break;
+                    case "blue":
+                        blueDice.status = "disabled";
+                        Console.WriteLine("Blue has been Disabled!");
+                        break;
+                    case "no":
+                        Console.WriteLine("Well, I will go now.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
         }
     }
     
