40157577def2196ad56bbd5d3b28600a85e3276f
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-12-04 16:24:38 -0700

    Update Main.cs
    a progress save

--- Main.cs
+++ Main.cs
@@ -1,5 +1,5 @@
 // Made by Benjamin Espenschied
-// V1.3
+// V1.4
 
 using System;
 
@@ -13,10 +13,12 @@ namespace myProgram
             string keepGoing = "yes";
             
             // Set up dice
-            Dice redDice = new Dice(6);
-            Dice yellowDice = new Dice(6);
-            Dice greenDice = new Dice(6);
-            Dice blueDice = new Dice(6);
+            Dice redDice = new Dice(6, "f");
+            Dice yellowDice = new Dice(6, "f");
+            Dice greenDice = new Dice(6, "b");
+            Dice blueDice = new Dice(6, "b");
+            Dice whiteDice1 = new Dice(6, "f");
+            Dice whiteDice2 = new Dice(6, "f");
             
             // Introductions
             Console.WriteLine("Hello! My name is Josh. I help work the Quixx dice. If \nYou want to roll the dice, just press enter. Typing \nin a dice color or no, then it will disable that \ndice or stop the program. Go ahead! Give it a shot!");
@@ -74,6 +76,22 @@ namespace myProgram
                         Console.ReadLine();
                         break;
                 }
+                
+                // Roll!
+                redDice.rollDie();
+                yellowDice.rollDie();
+                greenDice.rollDie();
+                blueDice.rollDie();
+                whiteDice1.rollDie();
+                whiteDice2.rollDie();
+                
+                redDice.comboWhite(whiteDice1, whiteDice2);
+                yellowDice.comboWhite(whiteDice1, whiteDice2);
+                greenDice.comboWhite(whiteDice1, whiteDice2);
+                blueDice.comboWhite(whiteDice1, whiteDice2);
+                whiteDice1.comboWhite(whiteDice2, whiteDice2);
+                
+                
             }
         }
     }
@@ -83,20 +101,32 @@ namespace myProgram
         public int roll;
         public int sides;
         public int[] results = new int[2];
+        public int[] resultsFlipped = new int[2];
         public string status;
         public Random numGen = new Random();
+        private string sortDirection
         
-        public Dice(int _sides)
+        public Dice(int _sides, string _sortDirection)
         {
             roll = 0;
             status = "active";
             sides = _sides;
+            sortDirection = _sortDirection;
         }
         
         public int rollDie()
         {
             roll = numGen.Next(1, sides + 1);
             return roll;
+        }
+        
+        public void comboWhite(int white1, int white2)
+        {
+            results[0] = white1 + roll;
+            results[1] = white2 + roll;
+            Array.Sort(results);
+            resultsFlipped[0] = results[1];
+            resultsFlipped[1] = results[0];
         }
     }
 }
